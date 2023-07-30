a8eef10b261106552192134eb82b029239ff0a0b
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2023-07-30 11:26:41 +0530

    Added Multiple staff register

--- routes/staffRouter.js
+++ routes/staffRouter.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const staffController = require('../controllers/staffController');
-
+const multer = require('multer');
+// Create an instance of multer for handling file uploads
+const upload = multer({
+    limits: {
+        fileSize: 1048576 // 1 mb
+    }
+});
 router.post('/signup', authController.staffSignup);
 router.post('/login', authController.staffLogin);
 
@@ -17,4 +23,5 @@ router.route('/:id')
 router.use(authController.restrictTo("hod", "admin")); // router.use(authController.restrictTo(staffUpdateRoles));
 router.post('/assignRole', authController.assignRoles);
 router.post('/updateMentees', staffController.migrateMentees);
+router.post('/addStaffs', upload.single('file'),  authController.staffsSignup)
 module.exports = router;
