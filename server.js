35eb515a66a33ca50a82d4597d6cd9e2765e43ab
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2023-08-28 18:19:34 +0530

    uncommented server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv');
-// process.on('uncaughtException', err => {
-//   console.log(err.name, err.message);
-//   process.exit(1);
-// })
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  process.exit(1);
+})
 
 // Connecting env variables
 dotenv.config({ path: './config.env' });
